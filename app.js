be502ab676ca9e6c510eb51a43123c00fbe84d41
HowNLuo <xji6cl4p@gmail.com>
2021-07-20 18:55:22 +0800

    feat: add CRUD

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
+const Record = require('./models/record')
+const bodyParser = require('body-parser')
 
 const mongoose = require('mongoose')
 const db = mongoose.connection
@@ -14,8 +17,64 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
+app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
-  res.send('hello')
+  Record.find()
+    .lean()
+    .sort({ date: 'desc' })
+    .then((records) => res.render('index', { records }))
+    .catch((error) => console.errer(error))
+})
+
+app.get('/record/:id/edit', (req, res) => {
+  const id = req.params.id
+  Record.findById(id)
+    .lean()
+    .then((record) => res.render('edit', { record }))
+    .catch((error) => console.error(error))
+})
+
+app.post('/record/:id', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  const date = req.body.date
+  const category = req.body.category
+  const amount = req.body.amount
+  Record.findById(id)
+    .then((record) => {
+      record.name = name
+      record.date = date
+      record.category = category
+      record.amount = amount
+      return record.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch((error) => console.error(error))
+})
+
+app.get('/record/new', (req, res) => {
+  res.render('new')
+})
+
+app.post('/record/edit', (req, res) => {
+  const name = req.body.name
+  const date = req.body.date
+  const category = req.body.category
+  const amount = req.body.amount
+  return Record.create({ name, date, category, amount })
+    .then(() => res.redirect('/'))
+    .catch((error) => console.error(error))
+})
+
+app.post('/record/:id/delete', (req, res) => {
+  const id = req.params.id
+  Record.findById(id)
+    .then((record) => record.remove())
+    .then(() => res.redirect('/'))
+    .catch((error) => console.error(error))
 })
 
 app.listen(3000, () => {
